735b3dd2d4c35ce52af9feda931ca8eb88b2d3ed
kingmolnar <peter@kingmolnar.com>
2017-02-10 11:07:05 -0500

    new

---
+++ 01_Spark_Core_SQL/adventureworks_pandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+Empoyees = pd.read_excel('/home/data/AdventureWorks/Employees.xls')
+Territory = pd.read_excel('/home/data/AdventureWorks/SalesTerritory.xls')
+Customers = pd.read_excel('/home/data/AdventureWorks/Customers.xls')
+Orders = pd.read_excel('/home/data/AdventureWorks/ItemsOrdered.xls')
+
